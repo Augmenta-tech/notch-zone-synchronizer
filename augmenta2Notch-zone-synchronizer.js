6ac374f6a01d29b38fde37aadc5de759371733fb
paulinelvne <pauline.lavainne@insa-lyon.fr>
2023-09-06 10:42:13 +0200

    Clean up comments

--- augmenta2Notch-zone-synchronizer.js
+++ augmenta2Notch-zone-synchronizer.js
@@ -167,7 +167,7 @@ function syncShapeNodes(namecur, currentPosition, currentRotation, currentShape,
     currentNode.SetFloat('Transform.Position X', currentPosition[0] + currentSize[0]/2);
     currentNode.SetFloat('Transform.Position Y', currentPosition[1] + currentSize[1]/2);
     currentNode.SetFloat('Transform.Position Z', currentPosition[2] + currentSize[2]/2);
-    // Warning : Looks like there is a value transformation for rotation
+
     currentNode.SetFloat('Transform.Rotation Heading', currentRotation[0] * Math.PI / 180);
     currentNode.SetFloat('Transform.Rotation Pitch', currentRotation[1] * Math.PI / 180);
     currentNode.SetFloat('Transform.Rotation Bank', currentRotation[2] * Math.PI / 180);
